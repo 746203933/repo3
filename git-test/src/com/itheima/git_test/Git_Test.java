1f79b970dff75f27c63062516d53e66ce21c456d
mike.liu <mike@itheima.com>
2018-12-17 10:46:13 +0800

    src第二次提交

---
+++ git-test/src/com/itheima/git_test/Git_Test.java
@@ -0,0 +1,23 @@
+package com.itheima.git_test;
+
+public class Git_Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project1");
+        System.out.println("hello git project2");
+
+    }
+
+    public void test(){
+            int i=0;
+            i+=10;
+            i-=2;
+    }
+}
